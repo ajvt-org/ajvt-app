89adc2b1b7be71a5ec7e3cc96ab8d4dc51fb99d0
eminyous <youssouf.emine@gmail.com>
2026-08-23 11:26:38 -0400

    Remove the stray drift migration

--- prisma/migrations/20260823135121_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "Competition_bankId_idx";
-
--- DropIndex
-DROP INDEX "Donation_memberId_source_membershipYear_idx";
-
--- AlterTable
-ALTER TABLE "Member" ALTER COLUMN "membershipYear" SET DEFAULT EXTRACT(YEAR FROM now() AT TIME ZONE 'UTC')::INTEGER;
