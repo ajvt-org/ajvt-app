fb2808b386d588ec47ef74971425414e9ddf0311
eminyous <youssouf.emine@gmail.com>
2026-08-24 23:23:04 -0400

    Put the activity lists on the shared row body

--- src/lib/texts/index.ts
+++ src/lib/texts/index.ts
@@ -4,6 +4,7 @@ export { discipline } from "./discipline";
 export { financeReport } from "./financeReport";
 export { landingActivities } from "./landing";
 export { matchAdmin, standingsAdmin, statsAdmin } from "./matchAdmin";
+export { memberActivities } from "./memberActivities";
 export { memberMatches } from "./memberMatches";
 export { activityWorkspace, tournamentWorkspace } from "./workspace";
 export { photoUpload } from "./photoUpload";
