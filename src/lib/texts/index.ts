5ed37ba76493c2c792b6a53114a7c22cc53d2e6b
eminyous <youssouf.emine@gmail.com>
2026-09-07 00:56:43 -0400

    Move the notifications toggle strings into texts

--- src/lib/texts/index.ts
+++ src/lib/texts/index.ts
@@ -68,6 +68,7 @@ export { memberMatches } from "./memberMatches";
 export { myActivities } from "./myActivities";
 export { mvpVote } from "./mvpVote";
 export { navigation } from "./navigation";
+export { notificationsToggle } from "./notifications";
 export { activityWorkspace, tournamentWorkspace } from "./workspace";
 export { waitingRequests, samePerson } from "./waitingRequests";
 export { memberPage } from "./memberPage";
