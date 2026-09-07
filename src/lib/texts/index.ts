d1b54d3df1a737bc9409feb3b0f829970ed3f1e5
eminyous <youssouf.emine@gmail.com>
2026-09-07 19:13:23 -0400

    Ask the giver for a name and separately whether to show it

--- src/lib/texts/index.ts
+++ src/lib/texts/index.ts
@@ -50,6 +50,7 @@ export { daysTab } from "./daysTab";
 export { lists } from "./lists";
 export { discipline } from "./discipline";
 export { donate } from "./donate";
+export { donorNameChoice } from "./donorNameChoice";
 export { donationActions, donationEdit } from "./donationEdit";
 export { dataExport, dataPage, siteVisits } from "./dataPage";
 export { expenseDestinations, expenseForm, expenseProofs, expensesPage } from "./expenses";
